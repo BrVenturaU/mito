c03d059f30652bdcf10f70e425fc2f55eaacdb8a
Wendy Hernández <hwendy750@gmail.com>
2021-03-01 22:55:08 -0600

    pruebas

--- src/components/BrailleTranslatorForm.vue
+++ src/components/BrailleTranslatorForm.vue
@@ -5,23 +5,22 @@
                 <h2>Braille Translator</h2>
                 <div class="row mt-5">
                     <div class="col-sm-6">
-                        <select class="custom-select">
+                        <select class="custom-select" name="valueFrom" id="valueFrom">
                             <option selected>Select an option</option>
-                            <option value="1">Español</option>
-                            <option value="2">Braille</option>
+                            <option :value="1">Spanish</option>
+                            <option :value="2">Braille</option>
                         </select>
                     </div>
                     <div class="col-sm-6">
-                        <select class="custom-select">
+                        <select class="custom-select" name="valueResult" id="valueResult">
                             <option selected>Select an option</option>
-                            <option value="1">Español</option>
-                            <option value="2">Braille</option>
+                            <option :value="1">Spanish</option>
+                            <option :value="2">Braille</option>
                         </select>
                     </div>
                 </div>
             </b-col>
         </b-row>  
-
         <b-row class="m-3 mt-4">
             <b-col class="col-md-12">
                 <div>
@@ -32,48 +31,42 @@
                             rows="3"
                             no-resize
                             required
-                            v-model="text1"
+                            v-model="valueResult"
                             placeholder="Enter text"
                             :formatter="formatter">
-                        </b-form-textarea> 
-
-                        <blockquote class="blockquote text-center" >
-                            <p class="mb-0 blockquote-footer">{{text1}}</p>
-                        </blockquote>
-                    </b-form-group >
-
-                    
-                    
-                    
-
-                    
-                        
+                        </b-form-textarea>
+                         <b-form-input
+                            class="text-center text-dark input-size mt-3"
+                            v-model="valueResult"
+                            type="text"
+                            readonly
+                            style="font-size: 2rem">
+                        {{ valueResult }}
+                        </b-form-input>
+                    </b-form-group >        
                 </div>
-                
             </b-col>
         </b-row>
-        <!--
-        <b-row class="m-3">
-            <b-col class="col-sm-12">
-                <div class="row bt">
-                    <div class="col-sm-12">
-                        <button type="button" class="btn btn-primary">Translate</button>
-                    </div>
-                </div> 
-            </b-col>
-        </b-row>-->
-            
     </div>
+    
 </template>
 
-
 <script>
+import br from 'braille'
+
+
+let code = br.toBraille('ANOTHER WORD'); //  ⠑⠭⠁⠍⠏⠇⠑⠀⠞⠑⠭⠞⠀⠂⠆⠒
+let text = br.toText('⠁⠝⠕⠞⠓⠑⠗⠀⠺⠕⠗⠙'); //  ANOTHER WORD
+console.log(code + ' ' + text)
+
+
+
 export default {
     name: 'BrailleTranslatorForm',
     data() {
       return {
-        text1: '',
-        text2: ''
+          valueFrom: '',
+          valueResult: ''
       }
     },
     methods: {
@@ -84,21 +77,5 @@ export default {
 };
 </script>
 <style scoped>
-
-blockquote {
-    margin-top: -50px;
-    padding-top: -100px;
-    border-radius: 5px;
-    margin-top: 3rem;
-    color:white;
-    width: 100%;
-    font-size:50px;
-    border-left:none;
-    background-color: rgba(200, 200, 200, 0.2); 
-}
-
-
-
-
 
 </style>
